401b2b898c479cae81592f61b319e43cf2e82509
luffy981 <jhonsmith981@hotmail.com>
2022-01-03 17:33:35 -0500

    fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # Install a package
 package { 'puppet_lint':
-    ensure   => '2.1.1',
+    ensure   => '2.5.0',
     name     => 'puppet-lint',
     provider => 'gem',
+  source     => 'www.rubygems.org'
 }
