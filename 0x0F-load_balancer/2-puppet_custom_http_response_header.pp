df52016a345dce9c92639d07280682590a49a246
luffy981 <jhonsmith981@hotmail.com>
2022-01-10 21:07:32 -0500

    using puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,30 @@
+# Pupper manifet to install nginx and others requirements
+
+$str = "add_header X-Served-By ${hostname};"
+
+exec { 'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package { 'nginx':
+  ensure  => installed,
+  require => Exec['update']
+}
+-> file_line { 'Add redirection, 301':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://twitter.com/Luffy_981 permanent;',
+}
+-> file_line { 'Add custom HTTP server':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => $str,
+}
+-> file { '/var/www/html/index.html':
+  content => 'Holberton School',
+}
+-> service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
