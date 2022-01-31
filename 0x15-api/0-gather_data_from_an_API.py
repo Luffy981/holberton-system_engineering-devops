8720d9660587ff7cde2582d146c0592f66b5b76d
luffy981 <jhonsmith981@hotmail.com>
2022-01-31 18:05:48 -0500

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,16 +6,24 @@ returns information about his/her TODO list progress.
 import requests
 from sys import argv
 
-user_id = argv[1]
-url = 'https://jsonplaceholder.typicode.com/'
-# GET  METHOD
-user = requests.get('{}users?id={}'.format(url, user_id)).json()[0]
-request = requests.get('{}todos?userId={}'.format(url, user_id)).json()
-name = user.get('name')
-tasks = len(request)
-# Method : List Comprehension
-task_list = [todo.get("title") for todo in request if todo.get('completed')]
-tasks_todo = len(task_list)
-string = "Employee {} is done with tasks({}/{}):"
-print(string.format(name, tasks, tasks_todo))
-[print("\t{}".format(i)) for i in task_list]
+
+def GET_Api():
+    """ GET data from API"""
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/'
+    # GET  METHOD
+    user = requests.get('{}users?id={}'.format(url, user_id)).json()[0]
+    request = requests.get('{}todos?userId={}'.format(url, user_id)).json()
+    name = user.get('name')
+    tasks = len(request)
+    # Method : List Comprehension
+    task_list = [todo.get("title") for todo in request
+                 if todo.get('completed')]
+    tasks_todo = len(task_list)
+    string = "Employee {} is done with tasks({}/{}):"
+    print(string.format(name, tasks, tasks_todo))
+    [print("\t{}".format(i)) for i in task_list]
+
+
+if __name__ == "__main__":
+    GET_Api()
