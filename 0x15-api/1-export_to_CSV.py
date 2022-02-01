06df3ec57b441103c37599facf23083c7b87c881
luffy981 <jhonsmith981@hotmail.com>
2022-01-31 20:43:23 -0500

    export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,9 +3,9 @@
 using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
+import csv
 import requests
 from sys import argv
-import csv
 
 
 def GET_Api():
