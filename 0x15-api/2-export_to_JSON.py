b3aa1efed8ceec9bcd1bdd559063ef85575a59d2
luffy981 <jhonsmith981@hotmail.com>
2022-01-31 21:46:14 -0500

    export to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import json
+import requests
+from sys import argv
+
+
+def GET_Api():
+    """ GET data from API"""
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/'
+    # GET  METHOD
+    user = requests.get('{}users?id={}'.format(url, user_id)).json()[0]
+    request = requests.get('{}todos?userId={}'.format(url, user_id)).json()
+    name = user.get('username')
+    tasks = len(request)
+    # Method : List Comprehension
+    task_list = [todo.get("title") for todo in request
+                 if todo.get('completed')]
+    tasks_todo = len(task_list)
+    string = "Employee {} is done with tasks({}/{}):"
+    print(string.format(name, tasks_todo, tasks))
+    [print("\t {}".format(i)) for i in task_list]
+    list_dict = []
+    for todo in request:
+        dict_to_json = {}
+        dict_to_json = {
+            'task': str(todo.get('title')),
+            'completed': str(todo.get('completed')),
+            'username': name,
+        }
+        list_dict.append(dict_to_json)
+    dict_to_json = {}
+    dict_to_json[user_id] = list_dict
+    print(dict_to_json)
+    filename = "{}.json".format(user_id)
+    with open(filename, 'w') as f:
+        json.dump(dict_to_json, f)
+
+
+if __name__ == "__main__":
+    GET_Api()
