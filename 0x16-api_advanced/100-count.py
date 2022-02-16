0a5389d6a12deb56874889de2ac8c96e3882ff2f
luffy <jhonsmith981@hotmail.com>
2022-02-15 22:31:48 -0500

    Count it git push

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,40 +1,73 @@
 #!/usr/bin/python3
-"""GET to api"""
-import requests
+"""This module makes a request to reddit api"""
+import requests as rq
+
+url = 'https://www.reddit.com/r/{}/hot/.json'
 
 
-def recurse(subreddit, word_list, after="", dic={}):
-    """recursive function"""
-    base_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    h = {'User-Agent': 'Reddit API test'}
-    params = {'limit': 200, 'after': after}
-    r = requests.get(base_url, headers=h, allow_redirects=False, params=params)
-    if r.status_code != 200:
+def fill_repetitions(posts=[], ind=0, word_count={}):
+    """Count words repetition"""
+    if ind >= len(posts):
+        return
+    post = posts[ind].get('data', {})
+    title = post.get('title', '').lower()
+    for t in title.split():
+        if t in word_count.keys():
+            word_count[t] += 1
+    fill_repetitions(posts, ind + 1, word_count)
+
+
+def get_posts(subreddit, word_count={}, params={}):
+    """"Fetches a lis of posts"""
+    headers = {'User-Agent': 'New agent 1.0'}
+    res = rq.get(url.format(subreddit), headers=headers, params=params,
+                 allow_redirects=False)
+
+    if res.status_code != 200:
         return None
-    d = r.json()
-    if after is None:
-        return dic
-    l = d.get('data', {}).get('children')
-    for i in l:
-        title = i.get('data', {}).get('title').lower().split()
-        for j in word_list:
-            for t in title:
-                if j == t:
-                    if j not in dic:
-                        dic[j] = 1
-                    else:
-                        dic[j] += 1
-    p = d.get('data', {}).get('after')
-    return recurse(subreddit, word_list, p, dic)
+
+    try:
+        body = res.json().get('data', None)
+        if body is None:
+            return None
+    except ValueError:
+        return None
+
+    updated_params = {
+        'after': body.get('after', None),
+        'count': body.get('dist', 0) + params.get('count', 0),
+        'limit': 100
+    }
+
+    fill_repetitions(body.get('children', []), 0, word_count)
+
+    if updated_params.get('after') is None:
+        return
+
+    return get_posts(subreddit, word_count, updated_params)
 
 
-def count_words(subreddit, word_list):
-    """Cound # of keywords"""
-    for i, e in enumerate(word_list):
-        word_list[i] = e.lower()
-    d = recurse(subreddit, word_list)
-    if d:
-        for key, value in sorted(d.items(), key=lambda x: (-x[1], x[0])):
-            print("{}: {}".format(key, value))
-    elif d is None:
-        print()
+def count_words(subreddit, word_list, word_count={}, params={}):
+    """This function fetches a reddit posts and prints
+    a word count"""
+    wl = [w.lower() for w in word_list]
+
+    word_list = list(set(wl))
+    word_count = {w: 0 for w in word_list}
+
+    get_posts(subreddit, word_count)
+
+    wc = {}
+    for w in wl:
+        wc[w] = wc.get(w, 0) + 1
+
+    reps = {}
+    for word, count in sorted(word_count.items()):
+        if count > 0:
+            ls = reps.get(count, [])
+            ls.append(word)
+            reps[count] = ls
+
+    for count, wlist in sorted(reps.items(), reverse=True):
+        for word in sorted(wlist):
+            print('{}: {}'.format(word, count * wc.get(word, 1)))
