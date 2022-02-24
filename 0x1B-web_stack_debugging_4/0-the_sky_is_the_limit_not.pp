e485f1633a94b3042d78900b96c4f13491b3e107
luffy <jhonsmith981@hotmail.com>
2022-02-23 19:14:48 -0500

    Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Debug done !
 
 exec { 'Change the limit':
-  command => 'sed -i s/15/4096/ /etc/default/nginx',
-  path    => '/bin'
+  command => 'sed -i s/15/4096/ /etc/default/nginx && sudo service nginx restart',
+  provider    => shell
 }
