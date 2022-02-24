676825261b4289cdfda3725e3b0c7a3b09ee971f
luffy <jhonsmith981@hotmail.com>
2022-02-23 20:29:57 -0500

    User limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,8 @@
+exec {'Updating limits to Holberton User':
+  command  => "sed -i 's/holberton soft nofile.*/holberton soft nofile 9000/' /etc/security/limits.conf",
+  provider => shell
+}
+exec {'Updating limit to Holberton User':
+  command  => "sed -i 's/holberton hard nofile.*/holberton hard nofile 9000/' /etc/security/limits.conf",
+  provider => shell
+}
